2e4c1175e01707aa14b29829c640553d07d50e16
Arman <Armantalebi79@gmail.com>
2023-11-25 17:00:04 +0300

    The base codes of components

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../Constants";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
