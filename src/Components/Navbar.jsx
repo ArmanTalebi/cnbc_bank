e7776dbf95a79d560d19e2c779c3a9ca744e01bc
Arman <Armantalebi79@gmail.com>
2023-11-26 13:06:05 +0300

    adding hamburger menu and navbar with useState hook

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className="{'font-poppins font-normal cursor-pointer'}"
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
           >
-            <a href="{'#{nav.id}'}" className="text-[#fff] text-[16px] mr-10">
-              {nav.title}
-            </a>
+            <a href={`#{nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
@@ -28,8 +28,27 @@ const Navbar = () => {
           src={toggle ? close : menu}
           alt="menu"
           className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
+          onClick={() => setToggle((prev) => !prev)}
         />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-indigo-950 absolute top-20 right-0 mx-4 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#{nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
