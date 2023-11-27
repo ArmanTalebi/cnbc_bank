2178404e0f33d6cb6a65caf9617a1716a83d9f05
Arman <Armantalebi79@gmail.com>
2023-11-27 19:37:01 +0300

    Removing on line from stats and business configues

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { features } from "../Constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Business = () => {
-  return <div>Business</div>;
-};
+const Business = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        you can do the Business, <br className="sm:block hidden" />
+        We'll handle the money.
+      </h2>
+      <p className={`${styles.paragraph}`}>
+        with the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds of
+        credit cards on the market.
+      </p>
+    </div>
+  </section>
+);
 
 export default Business;
--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -8,7 +8,6 @@ const Stats = () => (
         key={stat.id}
         className={`flex-1 flex justify-start items-center flex-row m-3`}
       >
-        //? {``} it means dynamic ???????
         <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
           {stat.value}
         </h4>
