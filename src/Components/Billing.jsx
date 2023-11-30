213af4796f066dfb61bdd9257274e93efa73250a
Arman <Armantalebi79@gmail.com>
2023-12-01 00:53:44 +0300

    some icons chaging in business and completed the billing

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -11,18 +11,30 @@ const Billing = () => (
       />
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
 
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>
-          Easily control your <br className="sm:block hidden" /> billing &
-          invoicing.
-        </h2>
-        //! yani ta sm dide she badesh hidden she
-        <p className={`${styles.paragraph} w-max-[470px] mt-5`}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
-          incidunt porro aliquam totam nam ratione blanditiis enim vel eius
-          quaerat.
-        </p>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing.
+      </h2>
+      //! yani ta sm dide she badesh hidden she
+      <p className={`${styles.paragraph} w-max-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
+        incidunt porro aliquam totam nam ratione blanditiis enim vel eius
+        quaerat.
+      </p>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img
+          src={apple}
+          alt="google_play"
+          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google_play"
+          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
+        />
       </div>
     </div>
   </section>
--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -4,14 +4,14 @@ import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
   <div
-    className={`flex flex-row p-6 rounded-[15px] ${
+    className={`flex flex-row p-6 rounded-[20px] ${
       index !== features.length - 1 ? "mb-6" : "mb-0"
     } feature-card cursor-pointer`}
   >
     <div
       className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
     >
-      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain " />
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain " />
     </div>
     <div className="flex-1 flex flex-col ml-3">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
@@ -28,15 +28,15 @@ const Business = () => (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        you can do the Business, <br className="sm:block hidden" />
+        you do the Business, <br className="sm:block hidden" />
         We'll handle the money.
       </h2>
-      <p className={`${styles.paragraph}`}>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         with the right credit card, you can improve your financial life by
         building credit, earning rewards and saving money. But with hundreds of
         credit cards on the market.
       </p>
-      <Button styles="mt-10" />
+      <Button styles={`mt-10`} />
     </div>
 
     <div className={`{layout.sectionImg} flex-col mt-3`}>
