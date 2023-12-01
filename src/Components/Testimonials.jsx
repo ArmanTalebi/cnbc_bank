8df493c695430fb4fc29108c21fc2961e3ed7202
Arman <Armantalebi79@gmail.com>
2023-12-01 17:52:10 +0300

    tesmonial and feedback cards completely added

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -13,6 +13,17 @@ const Testimonials = () => (
       <h1 className={styles.heading2}>
         What people are <br className="sm:block hidden" /> saying about us!
       </h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:justify-start justify-center feedback-container relative z-[1]">
+      {feedback.map((card) => (
+        <Feedback key={card.id} {...card} /> //? what is last paragraph for?
+      ))}
     </div>
   </section>
 );
